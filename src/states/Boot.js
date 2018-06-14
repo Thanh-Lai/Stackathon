bdafae34f282c106b809eafbc41f3143a21e607d
Thanh-Lai <tlai1122@gmail.com>
2018-06-14 13:38:03 -0400

    Added score, level, and lives texts

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -1,12 +1,22 @@
 import Phaser from 'phaser'
 import WebFont from 'webfontloader'
 import config from '../config'
+import globals from './globals'
+import { clone } from 'lodash'
 
 export default class extends Phaser.State {
   init () {
     this.stage.backgroundColor = '#262539'
     this.fontsReady = false
     this.fontsLoaded = this.fontsLoaded.bind(this)
+  }
+
+  create () {
+    this.initGlobalVariables()
+  }
+
+  initGlobalVariables () {
+    this.game.global = clone(globals)
   }
 
   preload () {
--- src/states/Game.js
+++ src/states/Game.js
@@ -5,7 +5,26 @@ export default class extends Phaser.State {
   preload () { }
 
   create () {
+    this.setUpText()
+  }
 
+  setUpText () {
+    this.createText(0, 20, 'Welcome', '50px Ariel', '#ffffff', 'center')
+    this.createText(20, 50, `Score: ${this.game.global.score}`, '20px Ariel', '#ffffff', 'left')
+    this.createText(20, 75, `Level: ${this.game.global.level}`, '20px Ariel', '#ffffff', 'left')
+    this.createText(20, 100, `Lives: ${this.game.global.lives}`, '20px Ariel', '#ffffff', 'left')
+  }
+
+  createText (xOffset, yOffset, text, font, color, align) {
+    return this.game.add.text(
+      xOffset,
+      yOffset,
+      text,
+      {
+        font: font,
+        fill: color,
+        boundsAlignH: align}
+    ).setTextBounds(0, 0, this.game.world.width, 0)
   }
 
   render () {
